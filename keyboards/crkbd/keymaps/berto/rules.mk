55d64a859fbc77506be8827ed249b48eca953bbc
Roberto Aguilar <r@rreboto.com>
2025-02-06 12:19:27 -0500

    Update rules.mk to enable features

--- keyboards/crkbd/keymaps/berto/rules.mk
+++ keyboards/crkbd/keymaps/berto/rules.mk
@@ -2,12 +2,12 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
-RGBLIGHT_ENABLE     = yes
+RGBLIGHT_ENABLE     = no
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = no
-COMBO_ENABLE        = no
+COMBO_ENABLE        = yes
 
 QMK_SETTINGS        = no
