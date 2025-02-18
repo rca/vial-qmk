5cd8ac1d0ac8fcab808d3cecf491d173baa51164
Roberto Aguilar <r@rreboto.com>
2025-02-18 09:20:13 -0500

    Fix equals button

--- keyboards/sofle/keymaps/berto/keymap.c
+++ keyboards/sofle/keymaps/berto/keymap.c
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                             KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
   _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
   KC_GRV,   KC_PIPE,  KC_MINS,  KC_LBRC,  KC_RBRC,  KC_PGUP,                           KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,
-  _______,  DF(0),    DF(1),    MO(3),    MO(4),    KC_PGDN, _______,        _______,  KC_MINS,  _______,  _______,  _______,  _______,  KC_ENT,
+  _______,  DF(0),    DF(1),    MO(3),    MO(4),    KC_PGDN, _______,        _______,  KC_EQL,   _______,  _______,  _______,  _______,  KC_ENT,
                       _______,  _______,  _______,  _______, _______,        _______,  _______,  _______,  _______,  _______
 ),
 /* RAISE
