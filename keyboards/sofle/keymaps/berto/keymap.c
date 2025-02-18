e174df723dee130ec4129c28cef543abecd1045a
Roberto Aguilar <r@rreboto.com>
2025-02-18 12:41:01 -0500

    Put option in the bottom-left corner

--- keyboards/sofle/keymaps/berto/keymap.c
+++ keyboards/sofle/keymaps/berto/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LAlt  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      | LAlt | LCTR |CMD   | /SHIFT  /       \LT(2, \  |SPACE |MO(2) |      |      |
  *            |      |      |      |      |/       /         \ENTER)\ |      |      |      |      |
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TD_ESC_CAPS_WORD,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
   KC_TD_ESC_CAPS_WORD,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                              KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
   KC_TD_TAB_LAYER_LOCK,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                              KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-  KC_LSFT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,       XXXXXXX,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+  KC_LALT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,       XXXXXXX,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                    _______,  KC_LALT,  KC_LCTL,  KC_LGUI,  KC_LSFT,       KC_L2_ENT,KC_SPC,   MO(2),    _______,  _______
 ),
 /*
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | TAB  |   A  |   R  |   S  |   T  |   D  |-------.    ,-------|   H  |   N  |   E  |   I  |   O  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
+ * |LAlt  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   K  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      | LAlt | LCTR |CMD   | /SHIFT  /       \LT(2, \  |SPACE |MO(2) |      |      |
  *            |      |      |      |      |/       /         \ENTER)\ |      |      |      |      |
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TD_ESC_CAPS_WORD,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSPC,
   KC_TD_ESC_CAPS_WORD,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,                              KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_BSPC,
   KC_TD_TAB_LAYER_LOCK,  KC_A,     KC_R,     KC_S,     KC_T,     KC_D,                              KC_H,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
-  KC_LSFT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,       XXXXXXX,  KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+  KC_LALT,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,       XXXXXXX,  KC_K,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
                                    _______,  KC_LALT,  KC_LCTL,  KC_LGUI,  KC_LSFT,       KC_L2_ENT,KC_SPC,   MO(2),    _______,  _______
 ),
 
