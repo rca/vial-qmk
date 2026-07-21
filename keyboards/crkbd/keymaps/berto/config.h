69b8ef802aefa229fda8279b245cd5b25d967d5d
Roberto Aguilar <r@rreboto.com>
2026-07-21 13:45:52 -0400

    Increase TAPPING_TERM
    Combined with PERMISSIVE_HOLD, this should make triggering `ctrl` on the
    tab key easier.

--- keyboards/crkbd/keymaps/berto/config.h
+++ keyboards/crkbd/keymaps/berto/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-#define TAPPING_TERM 130
+#define TAPPING_TERM 170
 
 // From QMK docs on Home Row Mods:
 // https://docs.qmk.fm/tap_hold#configuring-home-row-mods
